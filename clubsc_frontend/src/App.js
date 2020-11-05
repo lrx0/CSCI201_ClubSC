32767741cd9dcb2dc36f56b44edd75e9a806862e
Catherine Phu <chphu@usc.edu>
2020-11-04 23:10:30 -0800

    home page v2; Catherine

--- clubsc_frontend/src/App.js
+++ clubsc_frontend/src/App.js
@@ -2,16 +2,33 @@ import React from "react";
 import './App.css';
 import SearchBar from './SearchBar'; 
 import logo from "./artifacts/logo.svg"; 
+import clubLoginLogo from "./artifacts/clubLogin.svg"; 
+import studentLoginLogo from "./artifacts/studentLogin.svg"; 
 
 class App extends React.Component {
+  handleClubClick() {
+    console.log("Club is Being Logged In!"); 
+  }
+  
+  handleStudentClick() {
+    console.log("Student is Being Logged In!"); 
+  }
+
   render() {
     return (
       <div className="app body center">
         <img src={logo} alt="ClubSCLogo" className="center centered" id="jumbo-logo"/> 
-        <SearchBar className="search-from"/>
+        <div className="center" id="search">
+          <SearchBar/>
+        </div>
+        <div className="center" id="login-buttons">
+          <img src={clubLoginLogo} alt="ClubLoginLogo" className="login-button-padding" height="130px" onClick={this.handleClubClick}/>
+          <img src={studentLoginLogo} alt="StudentLoginLogo"  height="130px" onClick={this.handleStudentClick}/>
+        </div>
       </div>
     ); 
   }
 }
+
 
 export default App;
