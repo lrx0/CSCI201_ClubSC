4d1cace824be53b8e9e489f4629e6de8f253958e
Catherine Phu <chphu@usc.edu>
2020-11-04 21:50:06 -0800

    home page v1; Catherine

--- clubsc_frontend/src/App.js
+++ clubsc_frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"; 
 import './App.css';
+import SearchBar from './SearchBar'; 
 import logo from "./artifacts/logo.svg"; 
 
 class App extends React.Component {
   render() {
     return (
-      <div className="app">
-        <img src={logo} alt="logo" id="jumbo-logo"/> 
+      <div className="app body center">
+        <img src={logo} alt="ClubSCLogo" className="center centered" id="jumbo-logo"/> 
+        <SearchBar className="search-from"/>
       </div>
     ); 
   }
