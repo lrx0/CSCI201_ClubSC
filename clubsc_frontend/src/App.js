5bb5f56c91e9074892e0ca1db5ca62c0608abcf7
Catherine Phu <chphu@usc.edu>
2020-11-05 09:55:30 -0800

    home page add buttons; Catherine

--- clubsc_frontend/src/App.js
+++ clubsc_frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from "react"; 
 import './App.css';
 import SearchBar from './SearchBar'; 
+import CircleButton from './CircleButton'; 
 import logo from "./artifacts/logo.svg"; 
 import clubLoginLogo from "./artifacts/clubLogin.svg"; 
 import studentLoginLogo from "./artifacts/studentLogin.svg"; 
+import { View } from 'react-native';
 
 class App extends React.Component {
+
   handleClubClick() {
     console.log("Club is Being Logged In!"); 
   }
@@ -16,15 +19,15 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="app body center">
+      <div className="app center">
         <img src={logo} alt="ClubSCLogo" className="center centered" id="jumbo-logo"/> 
         <div className="center" id="search">
           <SearchBar/>
         </div>
-        <div className="center" id="login-buttons">
-          <img src={clubLoginLogo} alt="ClubLoginLogo" className="login-button-padding" height="130px" onClick={this.handleClubClick}/>
-          <img src={studentLoginLogo} alt="StudentLoginLogo"  height="130px" onClick={this.handleStudentClick}/>
-        </div>
+        <View style={{ flexDirection: 'row', justifyContent: 'center'}}>
+          <CircleButton onPress={this.handleClubClick}> <img src={clubLoginLogo} alt="ClubLoginLogo" height="130px" className="club-login-padding"/> </CircleButton>
+          <CircleButton onPress={this.handleStudentClick}> <img src={studentLoginLogo} alt="StudentLoginLogo"  height="130px" className="student-login-padding"/> </CircleButton>
+        </View>
       </div>
     ); 
   }
