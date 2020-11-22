1f0842d9a420c9eeab2f958afe18c55e0111c810
Lucas Huang <lmhuang@usc.edu>
2020-11-21 17:18:05 -0800

    Fixed Search Authentication

--- ClubSC/src/main/java/csci201/edu/usc/ClubSC/web/ClubSearchController.java
+++ ClubSC/src/main/java/csci201/edu/usc/ClubSC/web/ClubSearchController.java
@@ -1,29 +1,24 @@
 package csci201.edu.usc.ClubSC.web;
 import java.util.List;
-
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import csci201.edu.usc.ClubSC.domain.*;
 import csci201.edu.usc.ClubSC.payload.*;
 import csci201.edu.usc.ClubSC.service.ClubSearchService;
+
 @RestController
-@RequestMapping("/clubsearch")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/app")
 public class ClubSearchController
 {
     @Autowired
     ClubSearchService service;
  
-    @GetMapping
+    @PostMapping("/clubsearch")
     public ResponseEntity<List<Club>> getAllClubs( @Valid @RequestBody ClubSearchRequest ClubSearchRequest)
     {
         List<Club> list = service.getAllClubs(ClubSearchRequest.getKey());
