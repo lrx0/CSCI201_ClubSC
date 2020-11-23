50c525be2b89c8dff63886ff439a027189631822
Lucas Huang <lmhuang@usc.edu>
2020-11-23 00:58:03 -0800

    Fixes

---
+++ ClubSC/src/test/java/csci201/edu/usc/ClubSC/ClubScApplicationTests.java
@@ -0,0 +1,35 @@
+package csci201.edu.usc.ClubSC;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import csci201.edu.usc.ClubSC.web.*;
+
+@SpringBootTest
+class ClubScApplicationTests {
+	
+	@Autowired
+	private AuthController acontroller;
+	@Autowired
+	private ClubPageController pagecontroller;
+	@Autowired
+	private ClubPostController postcontroller;
+	@Autowired
+	private ClubSearchController searchcontroller;
+	@Autowired
+	private StudentFeedController feedcontroller;
+	@Autowired
+	private StudentFollowController followcontroller;
+
+	@Test
+	void contextLoads() {
+		assertThat(acontroller).isNotNull();
+		assertThat(pagecontroller).isNotNull();
+		assertThat(postcontroller).isNotNull();
+		assertThat(searchcontroller).isNotNull();
+		assertThat(feedcontroller).isNotNull();
+		assertThat(followcontroller).isNotNull();
+	}
+
+}
