0b60feea5dfdfb75ac26f3afcc5e0b23a2ef7440
Carlos Lao <crlao@usc.edu>
2020-11-23 07:54:26 -0800

    node_modules

--- ClubSC/src/main/frontend/src/pages/ClubPage.js
+++ ClubSC/src/main/frontend/src/pages/ClubPage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Faker from 'faker';
 import Post from '../components/Post';
 import axios from 'axios';
 import ContainerView from '../components/ContainerView';
