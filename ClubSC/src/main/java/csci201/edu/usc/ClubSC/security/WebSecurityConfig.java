314323684e06ce69a5f5c9949fdce39db23b477e
Carlos Lao <crlao@usc.edu>
2020-11-22 17:28:39 -0800

    Made changes Lucas asked

--- ClubSC/src/main/java/csci201/edu/usc/ClubSC/security/WebSecurityConfig.java
+++ ClubSC/src/main/java/csci201/edu/usc/ClubSC/security/WebSecurityConfig.java
@@ -56,8 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests()
-			.antMatchers("/api/auth/**").permitAll()
-			.antMatchers("/app/**").permitAll()
+			.antMatchers("/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
