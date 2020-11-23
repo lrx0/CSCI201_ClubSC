ffbbfa035b791006f286c796aca6c902dfa56f01
Carlos Lao <crlao@usc.edu>
2020-11-22 16:22:03 -0800

    removed HashRouter

--- ClubSC/src/main/frontend/src/index.js
+++ ClubSC/src/main/frontend/src/index.js
@@ -4,8 +4,6 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <App />,
   document.querySelector('#root')
 );
